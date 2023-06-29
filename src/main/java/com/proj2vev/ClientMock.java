91b95707ceaec0f677ab6b268f108909686bb096
Wilhelm Steins <wilhelmsteins@gmail.com>
2023-06-28 21:58:14 -0300

    updating mocking test

---
+++ src/main/java/com/proj2vev/ClientMock.java
@@ -0,0 +1,7 @@
+package com.proj2vev;
+
+public class ClientMock {
+  public String sendRequest(Request request) {
+   return "{\"slip\": { \"id\": 116, \"advice\": \"One of the top five regrets people have is that they didn't stay in contact with friends.\"}}";
+  }
+}
--- src/test/java/com/proj2vev/ClientControllerTest.java
+++ src/test/java/com/proj2vev/ClientControllerTest.java
@@ -8,7 +8,7 @@ import org.mockito.Mockito;
 public class ClientControllerTest {
   @Test
   public void mockitoTestClient() throws Exception {
-    Client clientMock = Mockito.mock(Client.class);
+    ClientMock clientMock = Mockito.mock(ClientMock.class);
     Request request = new Request();
     String expected = "{\"slip\": { \"id\": 116, \"advice\": \"One of the top five regrets people have is that they didn't stay in contact with friends.\"}}";
 
@@ -18,7 +18,7 @@ public class ClientControllerTest {
 
     String result = clientMock.sendRequest(request);
 
-    Mockito.verify(clientMock).sendRequest(request);
+    Mockito.verify(clientMock, Mockito.times(1)).sendRequest(request);
 
     assertEquals(
       "Mock test: ",
